00b51e7b6cdb0fcc869d0cde87e516a8fc08101d
Miro <Miro-wq@users.noreply.github.com>
2024-06-18 23:32:19 +0300

    first update hw #1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const allGalleryEl = galleryItems.map();
+
+const imageGallery = basicLightbox.create(
+  `<img src="${event.target.dataset.source}">`
+);
+imageGallery.show();
+
+const onKeydown = (e) => escapePress(e, imageGallery);
+
+document.addEventListener("keydown", onKeydown);
+
+// =========================DRAFT======================================
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `);
+// instance.show();
+// object.onkeydown = function () {
+//   myScript;
+// };
+// object.addEventListener("keydown", myScript);
+// elem.addEventListener("keydown", function (event) {
+//   // The parameter event is of the type KeyboardEvent
+//   addRow(event);
+// });
+// let msg = document.getDocumentById("#message");
+
+// msg.addEventListener("keydown", (event) => {
+//   // handle keydown
+// });
